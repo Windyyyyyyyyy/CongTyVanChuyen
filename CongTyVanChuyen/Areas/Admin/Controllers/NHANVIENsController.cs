d1424af4eef4a63836de99ffb54a7ac0a8bc4d0b
Phạm Tấn Hưng <90744736+tanhungg@users.noreply.github.com>
2023-10-24 00:05:58 +0700

    Phân luồng cho trang Admin

---
+++ CongTyVanChuyen/Areas/Admin/Controllers/NHANVIENsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CongTyVanChuyen.Models;
+
+namespace CongTyVanChuyen.Areas.Admin.Controllers
+{
+    public class NHANVIENsController : Controller
+    {
+        private CongTyVanChuyenEntities db = new CongTyVanChuyenEntities();
+
+        // GET: Admin/NHANVIENs
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.NHANVIENs.ToListAsync());
+        }
+
+        // GET: Admin/NHANVIENs/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = await db.NHANVIENs.FindAsync(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: Admin/NHANVIENs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NHANVIENs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "MaNV,TenNV,NgaySinh,DiaChi,SDT,CCCD")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NHANVIENs.Add(nHANVIEN);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(nHANVIEN);
+        }
+
+        // GET: Admin/NHANVIENs/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = await db.NHANVIENs.FindAsync(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: Admin/NHANVIENs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MaNV,TenNV,NgaySinh,DiaChi,SDT,CCCD")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nHANVIEN).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: Admin/NHANVIENs/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = await db.NHANVIENs.FindAsync(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: Admin/NHANVIENs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            NHANVIEN nHANVIEN = await db.NHANVIENs.FindAsync(id);
+            db.NHANVIENs.Remove(nHANVIEN);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
