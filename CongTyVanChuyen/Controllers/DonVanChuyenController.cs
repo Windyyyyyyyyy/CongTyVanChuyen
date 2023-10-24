4f40a1de91327cfd530931ffed21a8fa08eaf916
ChiCuong002 <buichicuong6110@gmail.com>
2023-10-24 13:08:16 +0700

    Tao don van chuyen

---
+++ CongTyVanChuyen/Controllers/DonVanChuyenController.cs
@@ -0,0 +1,24 @@
+﻿using CongTyVanChuyen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CongTyVanChuyen.Controllers
+{
+    public class DonVanChuyenController : Controller
+    {
+        // GET: DonVanChuyen
+
+        public ActionResult TaoDonVanChuyen()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult AddDonVanChuyen(DONVANCHUYEN dvc)
+        {
+            return Json("add success");
+        }
+    }
+}
